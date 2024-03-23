34525cd4cd9f569dbdbf82fc7e0c7dd7a2ba4929
Rus Price <rus@anolet.com>
2024-03-23 00:00:00 -0400

    make public

---
+++ gen.sh
@@ -0,0 +1,3 @@
+framework="HQRxOperators"
+./class-dump-swift Payload/HQ-iOS.app/Frameworks/$framework.framework/$framework > Extracts/HQ-iOS/Frameworks/$framework.framework/$framework.h
+strings Payload/HQ-iOS.app/Frameworks/$framework.framework/$framework > Extracts/HQ-iOS/Frameworks/$framework.framework/$framework.txt
---
+++ genassets.sh
@@ -0,0 +1,3 @@
+framework="WordsBubbleState"
+./class-dump-swift Payload/HQ-iOS.app/Frameworks/$framework.framework/$framework > Extracts/HQ-iOS.app/Frameworks/$framework.framework/$framework.h
+strings Payload/HQ-iOS.app/Frameworks/$framework.framework/$framework > Extracts/HQ-iOS.app/Frameworks/$framework.framework/$framework.txt
